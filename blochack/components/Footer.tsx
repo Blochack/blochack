3e67b9437b6b0a98cae8bc8d448db45cee8616b7
WiseMrMusa <abdulkareemusa01@gmail.com>
2024-01-28 14:49:53 +0100

    frontend: copied the frontend to organization

---
+++ blochack/components/Footer.tsx
@@ -0,0 +1,20 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+export const Footer = () => {
+    return (
+        <div className="bg-primary flex items-center justify-center mx-auto px-4 sm:px-6 lg:px-8 py-20 ">
+            <div className="text-center gap-4 max-w-md space-y-4">
+
+                <h1 className="text-4xl font-bold ">Information is Safety</h1>
+                <p className="text-balance">Protect yourself, get the information about the latest hack and safeguard yourself</p>
+                    <Input
+                        className="bg-primary rounded-3xl placeholder:text-primary-foreground text-primary-foreground"
+                        placeholder="Your email address"
+                    />
+                    <Button variant="secondary" className="w-full text-primary">Subscribe</Button>
+
+            </div>
+        </div>
+    );
+};
---
+++ blochack/components/sections/DontTrustVerify.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import { Container } from "@/components/container";
+import { TransactionsIllustration } from "@/components/trans";
+import { Button } from "@/components/ui/button";
+import { VerifyIllustration } from "../illustrations/veri";
+
+export const DontTrustVerify = () => {
+    return (
+        <div className="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36">
+            <Container>
+                <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20 ">
+
+                    <div className="relative z-10 mx-auto max-w-2xl lg:col-span-6 lg:max-w-none lg:pt-6 xl:col-span-6">
+                        <h1 className="text-4xl font-bold text-balance tracking-tight ">
+                            Dont Trust, Verify
+                        </h1>
+
+                        <div className="mt-6 flex flex-col gap-4">
+
+                        {
+                            [
+                                {
+                                    id: 1,
+                                    first: "Real-Time Decision-Making with Visual Trace",
+                                    second: "Real-time nature of visual transaction tracing in BlocHack ensures that users have up-to-the-minute insights into their "
+                                },
+                                {
+                                    id: 2,
+                                    first: "Proactive Security with Real-Time Alerts",
+                                    second: "Real-time nature of visual transaction tracing in BlocHack ensures that users have up-to-the-minute insights into their "
+                                },
+                                {
+                                    id: 3,
+                                    first: "Real-Time Decision-Making with Visual Trace",
+                                    second: "Real-time nature of visual transaction tracing in BlocHack ensures that users have up-to-the-minute insights into their "
+                                },
+                            ].map((e, i) => {
+                                return (
+                                    <div key={e.id} className="pr-8 py-4 space-y-2 border-b border-zinc-800">
+                                        <h1 className="text-lg font-semibold self-stretch text-balance">{e.first}</h1>
+                                        <p className="text-normal text-sm font-light self-stretch text-slate-300 text-balance h-16">{e.second}</p>
+                                    </div>
+                                );
+                            })
+                        }
+                        </div>
+
+                    </div>
+
+
+                    <div className="relative mt-10 sm:mt-20 lg:col-span-6 lg:row-span-2 lg:mt-0 xl:col-span-6">
+                        <VerifyIllustration className="w-full" />
+                    </div>
+                </div>
+
+
+            </Container>
+        </div>
+    );
+};
