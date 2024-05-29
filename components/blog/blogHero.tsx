18f75883b3a76f866eaf60bcfb6a8c6e6927e64d
MAC <mac@MACs-MacBook-Air.local>
2024-05-29 23:23:36 +0100

    blogs display page done?

--- components/blog/blogHero.tsx
+++ components/blog/blogHero.tsx
@@ -5,14 +5,14 @@ import { Container } from "@/components/container";
 
 export const BlogHero = () => {
     return (
-        <div className="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36">
+        <div className="overflow-hidden pt-20 sm:py-32 lg:pb-32 xl:pb-36">
             <Container>
                 <div className="">
-                    <div className="relative z-10">
-                        <h1 className="text-6xl font-bold text-balance tracking-tight ">
+                    <div className="relative z-10 w-6/12">
+                        <h1 className="text-5xl font-bold tracking-tight ">
                         Navigating the Crypto Security with BlocHack
                         </h1>
-                        <p className="mt-6 text-lg text-gray-600 text-balance w-3/5">
+                        <p className="mt-6 text-xl text-gray-600">
                         Meticulously curated repository of hacks and rugpulls, providing valuable insights into the evolving crypto threats.
                         </p>
                     </div>
--- components/blog/topics.tsx
+++ components/blog/topics.tsx
@@ -1,91 +1,101 @@
 import Link from "next/link";
 import { Container } from "@/components/container";
-import { BlogImgOne } from "../illustrations/BlogImg";
+import { BlogNav } from "@/components/blog/blogNav";
 // import { Button } from "./ui/button";
 
 export const Topics = () => {
     return (
-        <div className="overflow-hidden py-20 mb-60 sm:py-32 lg:pb-32 xl:pb-36">
+        <div className="overflow-hidden pb-20 mb-60 sm:pb-32 lg:pb-32 xl:pb-36">
             <Container>
+                <BlogNav />
                 <div className="grid gap-4 grid-cols-3">
                     <div className="mt-8">
                         <div className="relative bg-[url('/images/blogImg1.png')] rounded-xl h-[266px]">
-                            <p className="absolute right-4 top-4 bg-gray-800 py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
+                            <p className="absolute right-4 top-4 bg-[rgba(39,42,50,0.6)] text-sm py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
                         </div>
                         <div>
-                            <p className="text-xl w-4/5 border-b py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="text-xl w-4/5 py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="w-full h-2 border-b border-gray-800"></p>
                             <p className="mt-6">Learn More</p>
                         </div>
                     </div>
                     <div className="mt-8">
                         <div className="relative bg-[url('/images/blogImg2.png')] rounded-xl h-[266px]">
-                            <p className="absolute right-4 top-4 bg-gray-800 py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
+                            <p className="absolute right-4 top-4 bg-[rgba(39,42,50,0.6)] text-sm py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
                         </div>
                         <div>
-                            <p className="text-xl w-4/5 border-b py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="text-xl w-4/5 py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="w-full h-2 border-b border-gray-800"></p>
                             <p className="mt-6">Learn More</p>
                         </div>
                     </div>
                     <div className="mt-8">
                         <div className="relative bg-[url('/images/blogImg3.png')] rounded-xl h-[266px]">
-                            <p className="absolute right-4 top-4 bg-gray-800 py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
+                            <p className="absolute right-4 top-4 bg-[rgba(39,42,50,0.6)] text-sm py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
                         </div>
                         <div>
-                            <p className="text-xl w-4/5 border-b py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="text-xl w-4/5 py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="w-full h-2 border-b border-gray-800"></p>
                             <p className="mt-6">Learn More</p>
                         </div>
                     </div>
                     <div className="mt-8">
                         <div className="relative bg-[url('/images/blogImg1.png')] rounded-xl h-[266px]">
-                            <p className="absolute right-4 top-4 bg-gray-800 py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
+                            <p className="absolute right-4 top-4 bg-[rgba(39,42,50,0.6)] text-sm py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
                         </div>
                         <div>
-                            <p className="text-xl w-4/5 border-b py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="text-xl w-4/5 py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="w-full h-2 border-b border-gray-800"></p>
                             <p className="mt-6">Learn More</p>
                         </div>
                     </div>
                     <div className="mt-8">
                         <div className="relative bg-[url('/images/blogImg2.png')] rounded-xl h-[266px]">
-                            <p className="absolute right-4 top-4 bg-gray-800 py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
+                            <p className="absolute right-4 top-4 bg-[rgba(39,42,50,0.6)] text-sm py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
                         </div>
                         <div>
-                            <p className="text-xl w-4/5 border-b py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="text-xl w-4/5 py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="w-full h-2 border-b border-gray-800"></p>
                             <p className="mt-6">Learn More</p>
                         </div>
                     </div>
                     <div className="mt-8">
                         <div className="relative bg-[url('/images/blogImg3.png')] rounded-xl h-[266px]">
-                            <p className="absolute right-4 top-4 bg-gray-800 py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
+                            <p className="absolute right-4 top-4 bg-[rgba(39,42,50,0.6)] text-sm py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
                         </div>
                         <div>
-                            <p className="text-xl w-4/5 border-b py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="text-xl w-4/5 py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="w-full h-2 border-b border-gray-800"></p>
                             <p className="mt-6">Learn More</p>
                         </div>
                     </div>
                     <div className="mt-8">
                         <div className="relative bg-[url('/images/blogImg1.png')] rounded-xl h-[266px]">
-                            <p className="absolute right-4 top-4 bg-gray-800 py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
+                            <p className="absolute right-4 top-4 bg-[rgba(39,42,50,0.6)] text-sm py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
                         </div>
                         <div>
-                            <p className="text-xl w-4/5 border-b py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="text-xl w-4/5 py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="w-full h-2 border-b border-gray-800"></p>
                             <p className="mt-6">Learn More</p>
                         </div>
                     </div>
                     <div className="mt-8">
                         <div className="relative bg-[url('/images/blogImg2.png')] rounded-xl h-[266px]">
-                            <p className="absolute right-4 top-4 bg-gray-800 py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
+                            <p className="absolute right-4 top-4 bg-[rgba(39,42,50,0.6)] text-sm py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
                         </div>
                         <div>
-                            <p className="text-xl w-4/5 border-b py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="text-xl w-4/5 py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="w-full h-2 border-b border-gray-800"></p>
                             <p className="mt-6">Learn More</p>
                         </div>
                     </div>
                     <div className="mt-8">
                         <div className="relative bg-[url('/images/blogImg3.png')] rounded-xl h-[266px]">
-                            <p className="absolute right-4 top-4 bg-gray-800 py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
+                            <p className="absolute right-4 top-4 bg-[rgba(39,42,50,0.6)] text-sm py-2 px-4 bg-transparent-100 rounded-full">Partnership</p>
                         </div>
                         <div>
-                            <p className="text-xl w-4/5 border-b py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="text-xl w-4/5 py-4">Phishing Defense Powered by BlocHack</p>
+                            <p className="w-full h-2 border-b border-gray-800"></p>
                             <p className="mt-6">Learn More</p>
                         </div>
                     </div>
