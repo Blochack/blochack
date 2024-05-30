18c4ffaa7e41e5059312c4209687760fbebc9d3d
MAC <mac@MACs-MacBook-Air.local>
2024-05-30 16:09:13 +0100

    blog details jumbotron done?

--- components/blog/blogPageHero.tsx
+++ components/blog/blogPageHero.tsx
@@ -5,10 +5,24 @@ export const BlogPageHero = () => {
     return (
         <div className="overflow-hidden">
             <Container>
-                <div className="bg-[url('/images/blogjumbo.png')] bg-no-repeat h-screen">
-                    <h1 className="text-5xl font-bold tracking-tight">
-                    Phishing Defense Powered by BlocHack
-                    </h1>
+                <div className="bg-[url('/images/blogjumbo.png')] bg-no-repeat bg-bottom h-[65vh] py-44 relative">
+                   <div className="absolute bottom-0 w-full px-14 py-14">
+                        <h1 className="text-[52px] leading-tight tracking-tight pt-auto w-6/12">
+                        Phishing Defense Powered by BlocHack
+                        </h1>
+                        <div className="flex flex-row justify-between w-full mt-3">
+                            <div className="flex flex-row justify-start">
+                                <img className="flex-1" src="/images/bloguseravatar.png" alt="" />
+                                <div className="ml-2 flex-auto">
+                                    <p className="text-[#787D82] text-[10px]">AUTHOR</p>
+                                    <p className="text-[#787D82] text-base">George Oluwasegun</p>
+                                </div>
+                            </div>
+                            <div>
+                                <p className="text-[#787D82] text-base">November 2, 2023</p>
+                            </div>
+                        </div>
+                   </div>
                 </div>
             </Container>
         </div>
